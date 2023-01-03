b608b9a050d36980f3c13edff9239208e934bbee
Hung Nguyen <hungnguyencntt2808@gmail.com>
2023-01-03 22:02:01 +0700

    Update validation for get current user login

--- src/store/auth/authSaga.js
+++ src/store/auth/authSaga.js
@@ -8,21 +8,31 @@ function* handleLogin(action) {
         //yield delay(1000);
         const response = yield call(login, action.payload);
         localStorage.setItem('access_token', JSON.stringify(response.data.id_token));
-        yield put(authActions.loginSuccess({ ...action.payload }));
+
         const responseEmployee = yield call(getCurrentEmployeeLogin, action.payload);
         localStorage.setItem('role', responseEmployee.data.position);
         localStorage.setItem('current_employee_id', responseEmployee.data.id);
         localStorage.setItem('full_name', responseEmployee.data.user.firstName + ' ' + responseEmployee.data.user.lastName);
 
+        yield put(authActions.loginSuccess({ ...action.payload }));
         action.payload.onNavigate?.();
     } catch (error) {
         yield put(authActions.loginFailed(error));
-        yield put(
-            alertActions.showAlert({
-                text: 'The username or password you entered did not match our records. Please try again',
-                type: 'error'
-            })
-        );
+        if (error.response.status === 401) {
+            yield put(
+                alertActions.showAlert({
+                    text: 'The username or password you entered did not match our records. Please try again',
+                    type: 'error'
+                })
+            );
+        } else if (error.response.status === 400) {
+            yield put(
+                alertActions.showAlert({
+                    text: 'The user login does not match with any employee in database',
+                    type: 'error'
+                })
+            );
+        }
     }
 }
 
