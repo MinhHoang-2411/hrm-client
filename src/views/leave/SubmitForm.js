aa6a6f4e705c07a51a15c574d2e06484a50686f9
Hung Nguyen <hungnguyencntt2808@gmail.com>
2023-01-13 10:49:41 +0700

    Change length require of note

--- src/views/leave/SubmitForm.js
+++ src/views/leave/SubmitForm.js
@@ -716,8 +716,8 @@ const SubmitForm = ({ ...others }) => {
                 }}
                 validationSchema={Yup.object().shape({
                     note: Yup.string()
-                        .min(10, 'Please enter between 10 and 255 characters')
-                        .max(255, 'Please enter between 10 and 255 characters')
+                        .min(10, 'Please enter between 10 and 50 characters')
+                        .max(50, 'Please enter between 10 and 50 characters')
                         .required('Please enter Note')
                 })}
                 onSubmit={async (values, { setErrors, setStatus, setSubmitting, resetForm }) => {
