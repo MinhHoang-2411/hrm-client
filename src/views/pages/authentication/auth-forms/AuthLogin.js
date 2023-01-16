189defea9ef44ba0df2a33caacc44203b1870c47
Hung Nguyen <hungnguyencntt2808@gmail.com>
2023-01-16 13:40:22 +0700

    Change position of icon show password

--- src/views/pages/authentication/auth-forms/AuthLogin.js
+++ src/views/pages/authentication/auth-forms/AuthLogin.js
@@ -137,7 +137,7 @@ const FirebaseLogin = ({ ...others }) => {
                                             edge="end"
                                             size="large"
                                         >
-                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            {showPassword ? <Visibility /> : <VisibilityOff />}
                                         </IconButton>
                                     </InputAdornment>
                                 }
