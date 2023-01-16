d36297e73f0f9ceb0e718414cd99803651ea6d02
Hung Nguyen <hungnguyencntt2808@gmail.com>
2023-01-16 15:49:45 +0700

    Fix max length of username

--- src/utils/validate/login_schema.js
+++ src/utils/validate/login_schema.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 export const LoginSchema = (type) => {
     return Yup.object().shape({
-        username: Yup.string().max(25, 'Please no more than 25 characters').required('Please enter Username'),
+        username: Yup.string().max(50, 'Please no more than 50 characters').required('Please enter Username'),
         password: Yup.string().max(25, 'Please no more than 25 characters').required('Please enter Password')
     });
 };
